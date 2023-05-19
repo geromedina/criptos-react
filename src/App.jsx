845740d95805313af9aade19b45f9ba8cc53f27a
geromedina <gerocode768@gmail.com>
2023-05-19 17:20:59 -0300

    Añado formulario y styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,63 @@
 import { useState } from 'react'
+import styled from '@emotion/styled'
+import ImagenCripto from './img/imagen-criptos.png'
+import Formulario from './components/Formulario';
+
 
 
 function App() {
   
-
   return (
-    <h1>Desde App</h1>
+    <Contenedor>
+      <Imagen 
+      src={ImagenCripto}
+      alt='Imagen criptomonedas'
+      />
+      
+      <div>
+        <Heading>Cotiza Criptomonedas al Instante</Heading>
+        <Formulario />
+      </div>
+
+    </Contenedor>
   )
-}
+};
+
+const Contenedor = styled.div`
+    max-width: 900px;
+    margin: 0 auto;
+    @media (min-width: 992px) {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr);
+      column-gap: 2rem;
+    }
+`;
+  
+const Imagen = styled.img`
+  max-width: 400px;
+  width: 80%;
+  margin: 100px auto 0 auto;
+  display: block;
+`;
+
+const Heading = styled.h1`
+  font-family: 'Lato', sans-serif;
+  color: #FFF;
+  width: 90%;
+  text-align: center;
+  font-weight: 700;
+  margin-top: 80px;
+  margin-bottom: 50px;
+  font-size: 34px;
+
+  &::after {
+    content: '';
+    width: 100px;
+    height: 6px;
+    background-color: #66A2FE;
+    display: block;
+    margin: 10px auto 0 auto;
+  }
+`;
 
 export default App
